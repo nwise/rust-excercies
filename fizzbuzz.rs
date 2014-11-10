5f87acd76572ae3f0cf5cdf1cb5724035ab34473
Nathan Wise <nathanwise@gmail.com>
2014-11-10 07:19:30 -0500

    Initial commit

---
+++ fizzbuzz.rs
@@ -0,0 +1,53 @@
+fn main() {
+  for num in range(1i, 101) {
+    let mut answer =
+      if div_by_fifteen(num) { "FizzBuzz" }
+      else if div_by_three(num) { "Fizz" }
+      else if div_by_five(num) { "Buzz" }
+      else { num };
+
+    println!("{:s}", answer);
+  }
+}
+
+fn div_by_fifteen(num: int) -> bool {
+   num % 15 == 0
+}
+
+fn div_by_three(num: int) -> bool {
+  num % 3 == 0
+}
+
+fn div_by_five(num: int) -> bool {
+  num % 5 == 0
+}
+
+#[test]
+fn test_div_by_three() {
+  assert!(div_by_three(1), "One is not three");
+}
+
+#[test]
+fn test_div_by_three_with_three() {
+  assert!(!div_by_three(3), "Three should be three");
+}
+
+#[test]
+fn test_div_by_five() {
+  assert!( div_by_three(1), "One is not five");
+}
+
+#[test]
+fn test_div_by_five_with_five() {
+  assert!(!div_by_five(5), "Five should be five");
+}
+
+#[test]
+fn test_div_by_fifiteen(){
+  assert!(div_by_fifteen(1), "One is not fifteen");
+}
+
+#[test]
+fn test_div_by_fifiteen_with_fifteen(){
+  assert!(!div_by_fifteen(15), "Fifteen should be fifteen");
+}
